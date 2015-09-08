c37e480dc6042c85aba58dfedf036a3ff1587e4d
s.kato <0106.s.kato@gmail.com>
2015-09-08 15:55:51 +0900

    座席表人員配置 作業一時保存

--- WebContent/js/jointjs_seat_placement.js
+++ WebContent/js/jointjs_seat_placement.js
@@ -12,9 +12,6 @@ $(function() {
         isSaved = false;
     });
 
-	//var templateJSON = {"cells":[{"type":"basic.Rect","position":{"x":43,"y":30},"size":{"width":100,"height":60},"angle":0,"id":"f0d84580-915d-4ae9-b2a8-29d95d10edbe","z":1,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":143,"y":30},"size":{"width":100,"height":60},"angle":0,"id":"d62fd95e-9149-4f47-95ba-aa41f6c93343","embeds":"","z":2,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":243,"y":30},"size":{"width":100,"height":60},"angle":0,"id":"83d9bc08-b6ef-40d5-acbf-6d38d3c3e349","embeds":"","z":3,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":343,"y":30},"size":{"width":100,"height":60},"angle":0,"id":"93447b58-1fb3-4222-9fa1-8de0939c3789","embeds":"","z":4,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":343,"y":90},"size":{"width":100,"height":60},"angle":0,"id":"5aafe624-c6dc-4305-8cec-e6ad521a3750","embeds":"","z":5,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":243,"y":90},"size":{"width":100,"height":60},"angle":0,"id":"6dacadff-496d-4e54-9a96-022397cb06d6","embeds":"","z":6,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":143,"y":90},"size":{"width":100,"height":60},"angle":0,"id":"c374222c-d5eb-4908-9196-d471f1fdf6db","embeds":"","z":7,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":43,"y":90},"size":{"width":100,"height":60},"angle":0,"id":"ee7f002d-66c3-4b14-a659-0e9c2d121d0a","embeds":"","z":8,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":13,"y":178},"size":{"width":468,"height":10},"angle":0,"id":"c5c869b6-f213-4f5c-a72d-e92ca54bf6bf","z":9,"attrs":{"rect":{"fill":"orange"}}},{"type":"basic.Rect","position":{"x":43,"y":202},"size":{"width":100,"height":60},"angle":0,"id":"5ae13591-d528-4414-8c15-d59b176aba71","z":10,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":143,"y":262},"size":{"width":100,"height":60},"angle":0,"id":"08264866-114e-4cc5-b681-e121a78e2d2f","embeds":"","z":11,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":243,"y":202},"size":{"width":100,"height":60},"angle":0,"id":"c4d514db-9ca0-484d-9773-bb5b5aadc975","embeds":"","z":12,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":343,"y":262},"size":{"width":100,"height":60},"angle":0,"id":"d5e02bd7-e077-4d3d-8238-04574b22dd86","embeds":"","z":13,"attrs":{"rect":{"fill":"white"}}}]}
-	//graph.fromJSON(JSON.parse(templateJSON));
-
 	var cells = [];
 
 	/* 描画領域の作成 */
@@ -70,14 +67,9 @@ $(function() {
 		}
 	});
 
+    var templateJSON = '{"cells":[{"type":"basic.Rect","position":{"x":116,"y":80},"size":{"width":100,"height":60},"angle":0,"id":"7c73b6a6-9371-4eed-afd0-ed07327ad697","embeds":"","z":1,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":216,"y":80},"size":{"width":100,"height":60},"angle":0,"id":"c446f5a4-0a7d-431a-bbf1-5314f0eb63be","embeds":"","z":2,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":316,"y":80},"size":{"width":100,"height":60},"angle":0,"id":"445a183d-5840-423f-84b4-fa12a6acb2a9","embeds":"","z":3,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":116,"y":140},"size":{"width":100,"height":60},"angle":0,"id":"36e6bad8-bfd5-4437-8396-c56b9bda3890","embeds":"","z":4,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":216,"y":140},"size":{"width":100,"height":60},"angle":0,"id":"74374b01-d733-41ae-8099-1f0a82d8b47f","embeds":"","z":5,"attrs":{"rect":{"fill":"white"}}},{"type":"basic.Rect","position":{"x":316,"y":140},"size":{"width":100,"height":60},"angle":0,"id":"b2b5d7bf-324d-4fd1-96e8-b034069c4d8a","embeds":"","z":6,"attrs":{"rect":{"fill":"white"}}}]}';
+	graph.fromJSON(JSON.parse(templateJSON));
 
-	var desk = new joint.shapes.basic.Rect({
-		position : { x: 20, y:20 },
-		size : { width : 100, height : 60 },
-		attrs : { rect : { fill : 'white' } }
-	});
-
-	graph.addCell(desk);
 
 });
 /** $(function(){}); end * */
