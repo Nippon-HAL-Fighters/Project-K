412fa62d79047373fc581a0355f4ccde9c40e6c5
s.kato <0106.s.kato@gmail.com>
2015-09-10 21:20:05 +0900

    SeatTemplate.java

--- src/jp/ac/hal/tokyo/nippon_hal_fighters/controller/SeatTemplate.java
+++ src/jp/ac/hal/tokyo/nippon_hal_fighters/controller/SeatTemplate.java
@@ -2,6 +2,7 @@ package jp.ac.hal.tokyo.nippon_hal_fighters.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,24 +22,28 @@ import jp.ac.hal.tokyo.nippon_hal_fighters.dao.BackupDao;
 public class SeatTemplate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SeatTemplate() {
-        super();
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SeatTemplate() {
+		super();
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		BackupDao backupDao = new BackupDao();
 		BackupBean backupBean = new BackupBean();
 
@@ -48,6 +53,25 @@ public class SeatTemplate extends HttpServlet {
 
 		System.out.println(buckupString); // バックアップ情報の表示用
 
-	}
+		backupBean.setBackupFile(buckupString);
+		backupBean.setTitle("aaa");
+		backupBean.setResetDate("2015-01-01");
+		backupBean.setImplementor("ueno");
+		backupBean.setCompanyPlace("1");
 
+		try {
+			int success = backupDao.insertBuckup(backupBean);
+
+			if(success == 1) {
+				backupDao.commit();
+			} else {
+				backupDao.rollback();
+			}
+
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+	}
 }
